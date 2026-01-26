7aa17d01677ceb079c09ba1c1a33bfc743d39e2a
WolfySlayer <simon.mimos95@gmail.com>
2026-01-26 08:50:37 +0100

    Fix dependencies and Vercel build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   base: './'  
 })
+=======
+  base: './'
+})
+>>>>>>> 629dcf7 (Fix dependencies and Vercel build)
